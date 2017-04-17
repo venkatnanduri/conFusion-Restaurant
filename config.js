2d0803d2a9a74ee9d83b1e966dff1579943b1431
venkatnanduri <venkat.rajasekhar92@gmail.com>
2017-04-17 18:21:50 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'secretKey': '12345-67890-09876-54321',
+  'mongoUrl': 'mongodb://localhost:27017/conFusion',
+  'facebook':{
+    clientID: 435038806843199,
+    clientSecret: 'ad351c7c5edb599430ef67ac3666d1d5',
+    callbackURL: 'https://localhost:3443/users/facebook/callback'
+  }
+}
